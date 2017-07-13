cd7677d7a3467960b1c97c14da7ce41e8ad0abff
BG <fubinggui@aspirecn.com>
2017-07-13 09:52:25 +0800

    是这样的码

---
+++ 1-网易新闻/Classes/Home/Views/CIImageAccumulator+viewController.h
@@ -0,0 +1,13 @@
+//
+//  CIImageAccumulator+viewController.h
+//  1-网易新闻
+//
+//  Created by 丙贵 on 17/7/13.
+//  Copyright © 2017年 aspire. All rights reserved.
+//
+
+#import <CoreImage/CoreImage.h>
+
+@interface CIImageAccumulator (viewController)
+
+@end
