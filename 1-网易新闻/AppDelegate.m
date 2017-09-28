3009c2f40ade346898d0adce0a968d2dd6defa5a
BG <fubinggui@aspirecn.com>
2017-09-28 14:26:24 +0800

    bg2

--- 1-网易新闻/AppDelegate.m
+++ 1-网易新闻/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //123123124123213123
     return YES;
 }
 
